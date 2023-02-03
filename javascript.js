f2050c83f542e2f0a3c150afdb3d6efca85c5530
michaelmarkus4690 <116219341+michaelmarkus4690@users.noreply.github.com>
2023-02-03 15:46:05 -0800

    friday final product in classs

---
+++ javascript.js
@@ -0,0 +1,17 @@
+// Select Career Option
+function getOption() {
+    console.log(document.getElementById('selectacareer').value);
+  }
+
+
+// Gross Income
+
+// Gross Monthly Income, GA / 12
+
+// Default Deductions
+
+// TD = Sum Of Deductions
+
+// New Monthly = Gross Monthly - Total Deuctions
+
+// Current Balance = NM
